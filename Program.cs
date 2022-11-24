daf7e90a9cc2d8265671968061bab5a7b2e73bb1
baris <116105470+Lwoynk@users.noreply.github.com>
2022-11-24 23:09:45 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿using NLipsum.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,13 +13,193 @@ namespace Second_Project_Life_on_Mars
     {
         static void Main(string[] args)
         {
-
-
-
-            Operation3();
+            char[] operation3result = Operation3();
 
             //try
             //{
+
+            Console.WriteLine(operation3result);
+
+            Console.Write("enter number for x starting from: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("enter number for y how much: ");
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+
+            //operation 6
+            //char[] operation6result;
+            //operation6result = operation3result;
+
+            //for (int i = 0; i < operation6result.Length; i++)               
+            //{
+            //     char temp = operation6result[i];
+            //    if (operation6result[i] == 'A')
+            //        Console.Write('T');
+            //    else if (operation6result[i] == 'T')
+            //        Console.Write('A');
+            //    else if (operation6result[i] == 'G')
+            //        Console.Write('C');
+            //    else if (operation6result[i] == 'C')
+            //        Console.Write('G');
+            //    else
+            //        Console.Write(" ");
+
+            //}
+
+
+            //operation 8
+
+            //char[] operation8result;
+            //operation8result = operation3result;
+            ////                              28                                        9                                 17
+            //for (int i = 0; i < operation3result.Length; i++)    //int i = (((x-1) * 4) + 1); i < (((x-1) * 4) + 1) + (4 * y); i++
+            //{
+            //    if (i < (((x - 1) * 4) + 1)) 
+            //    {
+            //        operation8result[i] = operation3result[i];
+            //        Console.Write(operation8result[i]);
+            //    }
+            //    else if(i >= (((x - 1) * 4) + 1) + (4 * y)) 
+            //    {
+            //        operation8result[i- (((x - 1) * 4) + 1)] = operation3result[i];
+            //        Console.Write(operation8result[i - (((x - 1) * 4) + 1)]);
+            //    }
+            //}
+
+
+
+
+
+
+            //operation 9
+            //                              8
+            //DNA strand(stage 1) :  ATG ACT AGA TAT TGA
+            //Codon sequence: GAT GAG
+            //Starting from        :  3
+            //                                      16
+            //DNA strand(stage 2) :  ATG ACT GAT GAG AGA TAT TGA
+
+            //Console.Write("\nenter string for add: ");
+            //string k = Convert.ToString(Console.ReadLine());
+
+            //char[] ekleme = k.ToCharArray();
+
+            //char[] operation9result;
+            //operation9result = operation3result;
+
+            //for (int i = 0; i < 9; i++)
+            //{
+            //    operation3result[i] = operation9result[i];
+            //    Console.Write(operation9result[i]);
+            //}
+
+            //for (int l = 0; l < ekleme.Length; l++)
+            //{
+            //    operation9result[l + 9] = ekleme[l];
+            //    Console.Write(operation9result[l + 9]);
+            //}
+
+
+            //for (int j = 0; j <= operation3result.Length - 17; j++)
+            //{
+            //    operation3result[j + 9] = operation9result[16 + j];
+            //    Console.Write(operation9result[j + 16]);
+            //}
+
+
+
+
+
+
+
+
+            //operation 10   ***** yarısı bitti *****
+
+            //Console.Write("\nenter string for found: "); //******Bu kısım çalılşıyor else yok ama sadece 3 lü arıyor ******
+            //string z = Convert.ToString(Console.ReadLine());
+
+            //char[] arama = z.ToCharArray();
+            //int sayac = x;
+            //int sayac2 = 0;
+
+            //for (int i = (x - 1) * 4 + 1; i <= operation3result.Length - arama.Length; i++)
+            //{
+            //    if (operation3result[i] == arama[sayac2] /*&& operation3result[i + 1] == arama[1] && operation3result[i + 2] == arama[2]*/)
+            //    {
+            //        sayac++;
+            //        sayac2++;
+            //        Console.WriteLine("\nvar");
+            //        break;
+            //    }
+
+            //}
+
+            //if (operation3result[(x - 1) * 4 + 1] == arama[sayac2] /*&& operation3result[(x - 1) * 4 + 1 + 1] == arama[1] && operation3result[(x - 1) * 4 + 1 + 2] == arama[2]*/)
+            //{
+            //    Console.WriteLine(sayac - 1); 
+            //}
+            //else
+            //{
+            //    Console.WriteLine(sayac); 
+            //}
+
+
+            //else
+            //{
+            //    Console.WriteLine("yok");
+            //    break;
+            //}
+            //
+
+
+
+
+            //operation 11  
+
+            //if (y % 2 == 1)
+            //{
+            //    for (int i = ((x - 1) * 4) + 1; i < ((x - 1) * 4) + ((y / 2) * 4); i++)
+            //    {
+            //        char temp = operation3result[i];
+            //        operation3result[i] = operation3result[i + (y * 2) + 2];
+            //        operation3result[i + ((y * 2) + 2)] = temp;
+            //    }
+            //    Console.WriteLine(operation3result);
+            //}
+
+
+
+            //if (y % 2 == 0)
+            //{
+            //    for (int i = ((x - 1) * 4) + 1; i <= (((x - 1) * 4) - 1) + ((y / 2) * 4); i++)
+            //    {
+            //        char temp = operation3result[i];
+            //        operation3result[i] = operation3result[i + y*2];
+            //        operation3result[i + y*2] = temp;
+            //    }
+            //    Console.WriteLine(operation3result);
+            //}
+
+
+
+
+            //operation 12 *****BİTTİ****
+
+            //Console.Write("\nOperation 12");
+            //int counter = 0;
+            //for (int i = 0; i < operation3result.Length - 2; i++)
+            //{
+            //    if (operation3result[i] == 'A' && operation3result[i + 1] == 'T' && operation3result[i + 2] == 'G')
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //Console.WriteLine("\nNumber of genes: " + counter);
+
+
+
+
 
             //    //operation 1.load a dna sequence from a file
             //    string filepath = "";
@@ -51,7 +232,7 @@ namespace Second_Project_Life_on_Mars
         {
             try
             {
-                int abonds = 0, tbonds = 0, gbonds = 0, cbonds = 0, totalbonds = 0;
+                int abonds = 0, tbonds = 0, gbonds = 0, cbonds = 0, totalbonds = 0;  //bağ sayıları
 
                 char[] array = a.ToCharArray();
                 for (int i = 0; i < array.Length; i++)
@@ -128,6 +309,7 @@ namespace Second_Project_Life_on_Mars
                     {
                         codons[i] = ' ';
                     }
+
                 }
 
                 // genin start ve stop kodonlarini ekledik
@@ -135,9 +317,9 @@ namespace Second_Project_Life_on_Mars
                 codons[1] = 'T';
                 codons[2] = 'G';
                 codons[codons.Length - 3] = 'T';
-                codons[codons.Length - 2] = stop[random.Next(0,2)];
+                codons[codons.Length - 2] = stop[random.Next(0, 2)];
                 //TGG yi stop elemani olarak algilamamasi icin
-                if ( codons[codons.Length - 2]=='G')
+                if (codons[codons.Length - 2] == 'G')
                 {
                     codons[codons.Length - 1] = 'A';
                 }
@@ -150,7 +332,10 @@ namespace Second_Project_Life_on_Mars
             }
 
             // son olarak   GenderSelection()  fonskiyonundan gelen cinsiyeti genini diger genlerin uzerine yapistirir 
-            dna = MyFunctionForArrayConcatenation( GenderSelection() , dna );
+            //dna = MyFunctionForArrayConcatenation(GenderSelection(), dna);
+
+            //dna[15] = '';
+
             /////ve mucize gerceklesir/////
             return dna;
         }
@@ -166,32 +351,32 @@ namespace Second_Project_Life_on_Mars
             int gender_gene = random.Next(1, 7);
             if (gender_gene == 1)
             {
-                char[] f1 = { 'A', 'T', 'G', ' ', 'A', 'A', 'A', ' ', 'T', 'T', 'T', ' ', 'T', 'A', 'G'};
+                char[] f1 = { 'A', 'T', 'G', ' ', 'A', 'A', 'A', ' ', 'T', 'T', 'T', ' ', 'T', 'A', 'G' };
                 gender_array = f1;
             }
             else if (gender_gene == 2)
             {
-                char[] f1 = { 'A', 'T', 'G', ' ', 'T', 'T', 'T', ' ', 'A', 'A', 'A', ' ', 'T', 'A', 'G'};
+                char[] f1 = { 'A', 'T', 'G', ' ', 'T', 'T', 'T', ' ', 'A', 'A', 'A', ' ', 'T', 'A', 'G' };
                 gender_array = f1;
             }
             else if (gender_gene == 3)
             {
-                char[] f1 = { 'A', 'T', 'G', ' ', 'T', 'T', 'T', ' ', 'T', 'T', 'T', ' ', 'T', 'A', 'G'};
+                char[] f1 = { 'A', 'T', 'G', ' ', 'T', 'T', 'T', ' ', 'T', 'T', 'T', ' ', 'T', 'A', 'G' };
                 gender_array = f1;
             }
             else if (gender_gene == 4)
             {
-                char[] f1 = { 'A', 'T', 'G', ' ', 'T', 'T', 'T', ' ', 'C', 'C', 'C', ' ', 'T', 'A', 'G'};
+                char[] f1 = { 'A', 'T', 'G', ' ', 'T', 'T', 'T', ' ', 'C', 'C', 'C', ' ', 'T', 'A', 'G' };
                 gender_array = f1;
             }
             else if (gender_gene == 5)
             {
-                char[] f1 = { 'A', 'T', 'G', ' ', 'G', 'G', 'G', ' ', 'A', 'A', 'A', ' ', 'T', 'A', 'G'};
+                char[] f1 = { 'A', 'T', 'G', ' ', 'G', 'G', 'G', ' ', 'A', 'A', 'A', ' ', 'T', 'A', 'G' };
                 gender_array = f1;
             }
             else if (gender_gene == 6)
             {
-                char[] f1 = { 'A', 'T', 'G', ' ', 'G', 'G', 'G', ' ', 'T', 'T', 'T', ' ', 'T', 'A', 'G'};
+                char[] f1 = { 'A', 'T', 'G', ' ', 'G', 'G', 'G', ' ', 'T', 'T', 'T', ' ', 'T', 'A', 'G' };
                 gender_array = f1;
             }
             return gender_array;
@@ -215,7 +400,7 @@ namespace Second_Project_Life_on_Mars
         {
             //first_char_array.Length + second_char_array.Length + 1       +1 iki dizi arasinda bor bosluk birakmak icin
             char[] result = new char[first_char_array.Length + second_char_array.Length + 1];
-            
+
             //iki dizinin toplam sayisi ve +1 bosluk kadar calismasi gerekir
             for (int i = 0; i < (first_char_array.Length + second_char_array.Length + 1); i++)
             {
@@ -287,9 +472,5 @@ namespace Second_Project_Life_on_Mars
             return array;
 
         }
-
-
-
     }
-
 }
